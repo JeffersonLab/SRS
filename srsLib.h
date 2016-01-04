a5f3a30bdc17172db1607bcfed47f7635a195256
Bryan Moffit <moffit@jlab.org>
2016-01-04 16:33:26 -0500

    New higher level routines to set SRS parameters (DAQ IP, DTCC, ADC, ApvTriggerControl, EventBuild, Trigger Enable/Disable, APV reset).
    A few more to be written to take care of the rest of the stuff that
    was provided in the sample config files.

--- srsLib.h
+++ srsLib.h
@@ -11,6 +11,24 @@
 int   srsConnect(int *sockfd);
 int   srsReadBlock(int sockfd, volatile unsigned int* buf_in, int nwrds, int blocklevel);
 void  srsSetDebugMode(int enable);
+
+int   srsSetDAQIP(char *ip, char *daq_ip);
+int   srsSetDTCC(char *ip, int dataOverEth, int noFlowCtrl, int paddingType,
+		 int trgIDEnable, int trgIDAll, int trailerCnt, int paddingByte, int trailerByte);
+int   srsConfigADC(char *ip,
+		  int reset_mask, int ch0_down_mask, int ch1_down_mask,
+		  int eq_level0_mask, int eq_level1_mask, int trgout_enable_mask,
+		  int bclk_enable_mask);
+int   srsSetApvTriggerControl(char *ip,
+			     int mode, int trgburst, int freq,
+			     int trgdelay, int tpdelay, int rosync);
+int   srsSetEventBuild(char *ip,
+		       int chEnable, int dataLength, int mode, 
+		       int eventInfoType, unsigned int eventInfoData);
+int   srsTrigEnable(char *ip);
+int   srsTrigDisable(char *ip);
+int   srsAPVReset(char *ip);
+
 int   srsReadList(char *ip, int port, 
 		  unsigned int sub_addr, unsigned int *list, int nlist,
 		  unsigned int *data, int maxwords);
